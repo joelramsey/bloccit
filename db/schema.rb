1f5428811abc0f79ab03c433c1e1413b3ed8cc4e
joelramsey <joel.d.ramsey@gmail.com>
2015-03-15 00:20:23 +0000

    Completed crud assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311193545) do
+ActiveRecord::Schema.define(version: 20150314214418) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.string   "price"
+    t.string   "integer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
