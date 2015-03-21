8b2cb7a2ec7c5ee4bb2750a7739c6b4d40c9d986
joelramsey <joel.d.ramsey@gmail.com>
2015-03-21 00:27:33 +0000

    Associate and nested topics and posts

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,19 @@
-class PostPolicy < ApplicationPolicy
+class PostPolicy < ApplicationPolicy 
+
+  class Scope < Scope 
+    def resolve 
+      if user.admin? || user.moderator? 
+      scope.all
+
+      else
+      scope.where(:id => user.id).exists?
+      end
+    end
+  end 
+
+  
   def index?
-    true
+    user.admin? || user.id?
   end
+
 end
