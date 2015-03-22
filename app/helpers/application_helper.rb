d20437a717f1c021170ab5432d13b9cc89ee6f91
joelramsey <joel.d.ramsey@gmail.com>
2015-03-22 23:22:34 +0000

    Added application helper form-group-tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def my_name
     "Joel David Ramsey"
   end
+  
+  def form_group_tag(error, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
