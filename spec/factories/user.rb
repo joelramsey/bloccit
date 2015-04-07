faa647cf8d0933b59907c4defe6b7add9ec7bd36
joelramsey <joel.d.ramsey@gmail.com>
2015-04-07 03:35:03 +0000

    Complete testing users assignment

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,4 +6,16 @@ FactoryGirl.define do
     password_configuration "helloworld"
     confirmed_at Time.now
   end
+  
+  factory :user_with_post_and_comment do
+      transient do
+        comment_count 1
+      end
+
+      after(:create) do |user, evaluator|
+        post = create(:post, user: user)
+        create_list(:comment, evaluator.comment_count, post: post, user: user)
+       end
+    end
+  end
 end
